7f6f34116777f052343bb540f3fd0c0934e011a3
Brenton Niebauer <b.niebauer@me.com>
2024-09-16 17:42:00 -0400

    Initial Commit

---
+++ LessonTrackerTests/LessonTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  LessonTrackerTests.swift
+//  LessonTrackerTests
+//
+//  Created by Brenton Niebauer on 9/16/24.
+//
+
+import Testing
+@testable import LessonTracker
+
+struct LessonTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
